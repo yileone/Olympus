98b9195acb8d9300ce45a7a7ea654ae22c0ccef3
Anthonny Jose Marrufo <33264611+amarrufo@users.noreply.github.com>
2018-04-15 18:25:43 -0400

    subiendo war no queda system

--- workspace/Afrodita/src/com/jayktec/xyzOlympus/actions/ImportActionReg.java
+++ workspace/Afrodita/src/com/jayktec/xyzOlympus/actions/ImportActionReg.java
@@ -46,9 +46,9 @@ public class ImportActionReg extends ViewBaseAction implements IChangeController
 		assert origenOid == getOrigenOid();
 		assert sensorOid == getSensorOid();
 
-		System.out.println("    SENSOR : " + getSensorOid());
-		System.out.println("    ORIGEN : " + getOrigenOid());
-		System.out.println("");
+		// System.out.println(" SENSOR : " + getSensorOid());
+		// System.out.println(" ORIGEN : " + getOrigenOid());
+		// System.out.println("");
 		/*
 		 * System.out.println("*************************************");
 		 * System.out.println("");
@@ -67,7 +67,7 @@ public class ImportActionReg extends ViewBaseAction implements IChangeController
 			return;
 
 		} else {
-			System.out.println("PASO");
+			// System.out.println("PASO");
 		}
 		/* System.out.println("******************* Despues ******************"); */
 		@SuppressWarnings("resource")
